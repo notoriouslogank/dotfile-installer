718f4f118dcfac58c4892f6cebbb1a48313e8d43
logank <logan.aker1989@gmail.com>
2024-02-11 00:57:34 -0500

    Organized structure better.

--- install.sh
+++ install.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
+echo "Program start." >log.txt
 parent_path=$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
     pwd -P
 )
 cd "$parent_path"
-touch log.txt
 echo "Parent path: $parent_path" >>log.txt
-
-# Create backup directory
-if ! test -f ~/backups; then
-    mkdir ~/backups
-    echo "Created ~/backups dir." >>log.txt
-fi
 
 # Check if ~/.config exists and create it if not
 if ! test -f ~/.config; then
@@ -19,47 +13,52 @@ if ! test -f ~/.config; then
     echo "Created ~/.config dir." >>log.txt
 fi
 
-# Create backups for config files
+# Backups
+
+# ssh
 if test -f /etc/ssh/banner; then
-    sudo mv /etc/ssh/banner ~/backups/banner.bak
+    sudo mv /etc/ssh/banner backups/banner.bak
     echo "Backed up /etc/ssh/banner" >>log.txt
 fi
-
 if test -f /etc/ssh/ssh_config; then
-    sudo mv /etc/ssh/ssh_config ~/backups/ssh_config.bak
+    sudo mv /etc/ssh/ssh_config backups/ssh_config.bak
     echo "Backed up /etc/ssh/ssh_config." >>log.txt
 fi
-
 if test -f /etc/ssh/sshd_config; then
-    sudo mv /etc/ssh/sshd_config ~/backups/sshd_config.bak
+    sudo mv /etc/ssh/sshd_config backups/sshd_config.bak
     echo "Backed up /etc/ssh/sshd_config." >>log.txt
 fi
-
+# home_dir
 if test -f ~/.bashrc; then
-    sudo mv ~/.bashrc ~/backups/.bashrc.bak
+    sudo mv ~/.bashrc backups/.bashrc.bak
     echo "Backed up ~/.bashrc." >>log.txt
 fi
-
 if test -f ~/.zshrc; then
-    sudo mv ~/.zshrc ~/backups/.zshrc.bak
+    sudo mv ~/.zshrc backups/.zshrc.bak
     echo "Backed up ~/.zshrc." >>log.txt
 fi
-
 if test -f ~/.tmux.conf; then
-    sudo mv ~/.tmux.conf ~/backups/.tmux.conf.bak
+    sudo mv ~/.tmux.conf backups/.tmux.conf.bak
     echo "Backed up ~/.tmux.conf." >>log.txt
 fi
-
-# Update apt and install applications
+# .config
+if test -f ~/.config/alacritty; then
+    sudo mv ~/.config/alacritty backups/config/alacritty
+fi
+# applications
 declare -a apps=("neofetch" "ranger" "git" "bpytop" "htop" "zsh" "toilet" "figlet" "tmux" "curl" "cmake" "pkg-config" "libfreetype6-dev" "libfontconfig1-dev" "libxcb-xfixes0-dev" "libxkbcommon-dev" "python3" "zsh-doc" "scdoc")
+
+# Install applications
 sudo apt update -y
 for i in "${apps[@]}"; do
     type -p "$i" >/dev/null || (sudo apt install "$i" -y) && echo "Installed application: $i" >>log.txt
 done
 
-# Font(s)
-mkdir -p ~/.local/share/fonts
-echo "Created ~/.local/share/fonts" >>log.txt
+# Download and install fonts
+if ! test -f ~/.local/share/fonts; then
+    mkdir ~/.local/share/fonts
+    echo "Created ~/.local/share/fonts" >>log.txt
+fi
 cd ~/.local/share/fonts
 curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Mononoki/Regular/MononokiNerdFontMono-Regular.ttf
 curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Mononoki/Bold/MononokiNerdFontMono-Bold.ttf
@@ -67,7 +66,7 @@ curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Anonymo
 sudo cp * /usr/local/share/fonts
 cd $parent_path
 fc-cache -f -v
-echo "Installed font." >>log.txt
+echo "Installed fonts." >>log.txt
 
 # GitHub CLI
 if ! test -f /usr/bin/gh; then
@@ -79,22 +78,31 @@ if ! test -f /usr/bin/gh; then
     echo "Installed gh." >>log.txt
 fi
 
-# Rust
-curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh
-exec zsh
-source "$HOME/.cargo/env"
+# rustup
+echo "Checking for rustup..." >>log.txt
+if ! test -f ~/.cargo/bin/rustup; then
+    curl --protto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    echo "Installed rustup."
+fi
 
-# Alacritty
+# Clone and build Alacritty
+echo "Cloning Alacritty." >>log.txt
+cd $parent_path
 git clone https://github.com/alacritty/alacritty.git
 cd alacritty
+echo "Building Alacritty..." >>log.txt
 rustup override set stable
 rustup update stable
 cargo build --release
 infocmp alacritty
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp target/release/alacritty /usr/local/bin
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
+echo "Installed Alacritty." >>log.txt
+
+# Alacritty manpages
+echo "Bulding Alacritty manpages..." >>log.txt
 sudo update-desktop-database
 sudo mkdir -p /usr/local/share/man/man1
 sudo mkdir -p /usr/local/share/man/man5
@@ -104,37 +112,39 @@ scdoc <extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/
 scdoc <extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz >/dev/null
 
 # lsd
+echo "Cloning and building lsd..." >>log.txt
+cd $parent_path
 cargo install --git https://github.com/lsd-rs/lsd.git --branch master
 echo "Installed lsd." >>log.txt
+
 # Create banner
-sudo figlet -f pagga "$HOST" >~/banner
-sudo cp ~/banner /etc/ssh/banner
+cd $parent_path
+figlet -f pagga "$HOST" >>assets/etc/ssh/banner
+sudo cp assets/etc/ssh/banner /etc/ssh/banner
 echo "Created ssh banner." >>log.txt
-sudo rm ~/banner
 
 # Oh-my-zsh
-if ! test -f ~/.oh-my-zsh; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    echo "Installed oh-my-zsh." >>log.txt
-fi
+#if ! test -f ~/.oh-my-zsh; then
+#    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#    echo "Installed oh-my-zsh." >>log.txt
+#fi
 
-# Move config files to necessary destinations
-echo "Moving new config files." >>log.txt
-cd "$parent_path"
+# Config files
+
+# ssh
 sudo cp "$parent_path/assets/etc/ssh/ssh_config" "/etc/ssh/ssh_config"
-echo "Moved ssh_config." >>log.txt
-sudo cp "$parent_path/assets/etc/ssh/sshd_config" "/etc/ssh/sshd_config"
-echo "Moved sshd_config." >>log.txt
-sudo cp -r "$parent_path/assets/.config/bpytop" ~/.config
-echo "Moved bpytop." >>log.txt
-sudo cp -r "$parent_path/assets/.config/neofetch" ~/.config
-echo "Moved neofetch to .config." >>log.txt
-sudo cp -r "$parent_path/assets/.config/alacritty" ~/.config
-echo "Moved alacritty to .config." >>log.txt
-sudo cp -r "$parent_path/assets/home_dir/.zshrc" ~
-echo "Moved .bashrc to ~" >>log.txt
-sudo cp -r "$parent_path/assets/home_dir/.bashrc" ~
-echo "Moved .tmux.conf to ~" >>log.txt
-sudo cp -r "$parent_path/assets/home_dir/.tmux.conf" ~
-clear
+sudo cp "$parent_path/assetts/etc/ssh/sshd_config" "/etc/ssh/sshd_config"
+sudo cp "$parent_path/assets/etc/ssh/banner" "/etc/ssh/banner"
+echo "Created ssh configs." >>log.txt
+# .config
+cp -r "$parent_path/assets/config/alacritty" ~/.config
+cp -r "$parent_path/assets/config/bpytop" ~/.config
+cp -r "$parent_path/assets/config/neofetch" ~/.config
+echo "Created .config files." >>log.txt
+# home_dir
+cp -r "$parent_path/assets/home_dir/zshrc" ~/.zshrc
+cp -r "$parent_path/assetts/home_dir/bashrc" ~/.bashrc
+cp -r "$parent_path/assets/home_dir/tmux.conf" ~/.tmux.conf
+echo "Created shell config files." >>log.txt
+
 echo "Done.  Please see log.txt for more information."
